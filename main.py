1fe4bf0a055407d5796a8026ab2939dd8b83fa24
Toni Hermoso Pulido <toniher@cau.cat>
2023-06-24 19:17:59 +0200

    Update style

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import picamera2 #camera module for RPi camera
+import picamera2  # camera module for RPi camera
 from picamera2 import Picamera2
 from picamera2.encoders import JpegEncoder
 from picamera2.outputs import FileOutput
@@ -8,10 +8,11 @@ from libcamera import Transform
 import io
 from threading import Condition
 
-from flask import Flask, render_template, Response, request, send_from_directory
+from flask import Flask, render_template, Response
 
 # Code from: https://github.com/raspberrypi/picamera2/issues/366
 # Code from: https://github.com/EbenKouao/pi-camera-stream-flask
+
 
 class StreamingOutput(io.BufferedIOBase):
     def __init__(self):
@@ -23,12 +24,14 @@ class StreamingOutput(io.BufferedIOBase):
             self.frame = buf
             self.condition.notify_all()
 
-#defines the function that generates our frames
+
+# defines the function that generates our frames
 def genFrames():
-    #buffer = StreamingOutput()
+    # buffer = StreamingOutput()
     with picamera2.Picamera2() as camera:
         output = StreamingOutput()
-        camera.configure(camera.create_video_configuration(main={"size": (640, 480)}, transform=Transform(180)))
+        camera.configure(camera.create_video_configuration(main={
+            "size": (640, 480)}, transform=Transform(180)))
         output = StreamingOutput()
         camera.start_recording(JpegEncoder(), FileOutput(output))
         while True:
@@ -36,21 +39,25 @@ def genFrames():
                 output.condition.wait()
                 frame = output.frame
             yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 # App Globals (do not edit)
 app = Flask(__name__, template_folder='templates')
 
+
 @app.route('/')
 def index():
-    return render_template('index.html') # you can customze index.html here
+    return render_template('index.html')  # you can customze index.html here
 
-#defines the route that will access the video feed and call the feed function
+# defines the route that will access the video feed and call the feed function
+
+
 @app.route('/video_feed')
 def video_feed():
     return Response(genFrames(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 if __name__ == '__main__':
 
