05ed81696960f1397fb46fbd2bff3a58546910ad
Toni Hermoso Pulido <toniher@cau.cat>
2023-07-16 19:32:51 +0200

    Missing imports

--- forms.py
+++ forms.py
@@ -1,3 +1,7 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, BooleanField
+from wtforms.validators import DataRequired
+
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
