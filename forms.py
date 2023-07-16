1cb579eecc55cd331c0449a1c8c4afc804166b7f
Toni Hermoso Pulido <toniher@cau.cat>
2023-07-16 19:49:29 +0200

    Update

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, BooleanField
+from wtforms import BooleanField, StringField, PasswordField, SubmitField, validators
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
